e4866d13e568eb3db55baed60b28e031efd15112
Adilbae Ruslan <adilbaev-ruslan@umail.uz>
2023-01-04 23:11:48 +0500

    User model added comment

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+    // User model komment
     /**
      * The attributes that are mass assignable.
      *
