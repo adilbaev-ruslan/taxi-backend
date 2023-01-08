bbbc747f5967544c4e601a98acb118ef99ebf823
Adilbaev Ruslan <adilbaev-ruslan@umail.uz>
2023-01-08 20:07:43 +0500

    Dashboard update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,20 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+
+                @if(auth()->user()->role->name == 'Guest')
+                    {{ __("You're Guest logged in!") }}
+                @elseif(auth()->user()->role->name == 'Owner')
+                    {{ __("You're Owner logged in!") }}
+                @elseif(auth()->user()->role->name == 'Admin')
+                    {{ __("You're Admin logged in!") }}
+                @elseif(auth()->user()->role->name == 'Company')
+                    {{ __("You're Company logged in!") }}
+                @elseif(auth()->user()->role->name == 'Agent')
+                    {{ __("You're Agent logged in!") }}
+                @elseif(auth()->user()->role->name == 'Driver')
+                    {{ __("You're Driver logged in!") }}
+                @endif        
                 </div>
             </div>
         </div>
